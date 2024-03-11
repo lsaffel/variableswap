f738f7e23986c8b3304d8deb2b6da6e0f70ed0d6
Lisa <llvvss@icloud.com>
2024-03-10 21:54:42 -0600

    variable swap without using a temp variable

--- Source Code/variableswap.cpp
+++ Source Code/variableswap.cpp
@@ -7,10 +7,17 @@ int main()
     int a = 20;
     int b = 10;
 
-    // swap the variables using a temp variable
-    int temp = a;
-    a = b;
-    b = temp;
+    // // swap the variables using a temp variable
+    // int temp = a;
+    // a = b;
+    // b = temp;
+
+    // cout << "a = " << a << ", b = " << b << endl;
+
+    // swap the variables without using a temp variable
+    a = a + b;      // 30
+    b = a - b;      // 20
+    a = a - b;      // 10
 
     cout << "a = " << a << ", b = " << b << endl;
 }
